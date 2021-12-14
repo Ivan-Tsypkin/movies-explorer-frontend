082ee142a4a1a6b31532544778a8a4787aa76e8c
Ivan <bfg546@yandex.ru>
2021-12-14 14:22:37 +0300

    Правки после 1-ой проверки

--- src/components/Movies/SearchForm.js
+++ src/components/Movies/SearchForm.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+
 export default function SearchForm() {
+
+  const [isFocused, setIsFocused] = useState(false);
 
   return (
     <div className="search">
         <form className="search__form" method="POST" name="searchForm">
-          <div className="search__bar">
+          <div className={`search__bar ${isFocused ? 'search__bar_focused' : ''}`}>
             <input
               className="search__form-item"
               type="text"
@@ -13,11 +17,16 @@ export default function SearchForm() {
               minLength="2"
               maxLength="100"
               required
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
             ></input>
             <button className="search__submit-button" type="submit" aria-label="Найти">Найти</button>
           </div>
           <label className="search__checkbox-label">
-            <input className="search__checkbox" type="checkbox"></input>
+            <input
+              className="search__checkbox"
+              type="checkbox"
+            ></input>
             <span className="search__visible-checkbox"></span>
             Короткометражки
           </label>
